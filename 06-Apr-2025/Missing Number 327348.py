43b63031e1a7d791e7105dd04a0e6af459380483
Yihune Zewdie <99527614+yihune21@users.noreply.github.com>
2025-04-06 20:34:40 +0300

    Created 'Missing Number' by A2SV Hub

---
+++ 06-Apr-2025/Missing Number 327348.py
@@ -0,0 +1,9 @@
+# Problem: Missing Number - https://leetcode.com/problems/missing-number/description/
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+
+        for i in range(len(nums) + 1):
+            if i not in nums:
+                return i
+        
