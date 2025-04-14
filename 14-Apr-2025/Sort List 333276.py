b8a17e8e5cedfb50ed320a2762b2110a1320e271
Yihune Zewdie <99527614+yihune21@users.noreply.github.com>
2025-04-14 14:52:31 +0300

    Created 'Sort List' by A2SV Hub

---
+++ 14-Apr-2025/Sort List 333276.py
@@ -0,0 +1,36 @@
+# Problem: Sort List - https://leetcode.com/problems/sort-list/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        
+        if not head or not head.next:
+            return head
+        
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        mid = slow.next
+        slow.next = None
+        
+        left = self.sortList(head)
+        right = self.sortList(mid)
+        
+        dummy = ListNode(0)
+        tail = dummy
+        while left and right:
+            if left.val < right.val:
+                tail.next = left
+                left = left.next
+            else:
+                tail.next = right
+                right = right.next
+            tail = tail.next
+        tail.next = left if left else right
+        return dummy.next
